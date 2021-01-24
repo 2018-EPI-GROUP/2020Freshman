65058b8d00b6e77bda6ca429ff1badd03fa1a89b
AlexZhu2001 <670429759@qq.com>
2021-01-24 22:47:34 +0800

    Fix some problem

--- SDL2/SDL2-02/main.c
+++ SDL2/SDL2-02/main.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
-#define  WIDTH 400
-#define HEIGHT 300
+#define  WIDTH 800
+#define HEIGHT 600
 
 
-int event_loop() {
-    SDL_Event event;
-    SDL_PollEvent(&event);
-    switch(event.type)
-    {
-        case SDL_QUIT:return 0;
-        default: break;
+void event_loop() {
+    while(1){
+        SDL_Event event;
+        if (SDL_PollEvent(&event)) {//Have Event
+            switch (event.type) {
+                case SDL_QUIT://Closing Window
+                    return;
+                default:
+                    break;
+            }
+        }
     }
-    return 1;
 }
 
 int main() {
+    //Init
     if(SDL_Init(SDL_INIT_VIDEO) != 0){
         SDL_Log("Error,%s",SDL_GetError());
     }
+    //Create Window
     SDL_Window *win=SDL_CreateWindow("HelloWorld",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
+    //Get Surface Filled by White(ARGB)
+    SDL_Surface *screen=SDL_GetWindowSurface(win);
+    SDL_Rect r={0,0,WIDTH,HEIGHT};
+    SDL_FillRect(screen,&r,0xffffffff);
+    SDL_UpdateWindowSurface(win);
+    //Show Window EventLoop
     SDL_ShowWindow(win);
-    while(event_loop());
+    event_loop();
+    //DestoryWindow
+    SDL_DestroyWindow(win);
     return 0;
 }
